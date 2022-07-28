e2008017022b4c1fff3c086e392f8589164bb469
vankoosh <vankoosh@users.noreply.github.com>
2022-07-28 00:29:38 -0100

    started on certificates fade in

--- fadeInElementsOnScroll.js
+++ fadeInElementsOnScroll.js
@@ -1,9 +1,10 @@
 const fadeInElementsOnScroll = () => {
   const storyElem = document.querySelector("#story-container p");
   const cv = document.querySelector("#cv");
+  const skills = document.querySelector(".my-skills");
 
   // console.log(storyElem.getBoundingClientRect().top - window.scrollY);
-  console.log(cv.getBoundingClientRect().top - window.scrollY);
+  console.log(skills.getBoundingClientRect().top - window.scrollY);
 
   if (storyElem.getBoundingClientRect().top - window.scrollY < -50) {
     storyElem.style.opacity = "1";
@@ -13,6 +14,11 @@ const fadeInElementsOnScroll = () => {
   if (cv.getBoundingClientRect().top - window.scrollY < -200) {
     cv.style.marginTop = "5rem";
     cv.style.opacity = "1";
+  }
+
+  if (skills.getBoundingClientRect().top - window.scrollY < -800) {
+    skills.style.marginTop = "2rem";
+    skills.style.opacity = "1";
   }
 };
 
