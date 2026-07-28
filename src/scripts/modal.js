10765bda52fa20d273ee2d88248ec4d79f210b9d
Mario Nano <mario.nano.dev@gmail.com>
2026-07-28 15:46:19 +0200

    scroll lock on modal open, update social links

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -4,6 +4,7 @@ export default function modal() {
 			modalEl.style.display = "none";
 			document.documentElement.classList.remove("modal-open");
 			document.body.classList.remove("modal-open");
+			// eslint-disable-next-line no-undef
 			lenis.start();
 		});
 	});
@@ -14,9 +15,10 @@ export default function modal() {
 			const modalImg = modalEl.querySelector(".modal-content");
 			modalEl.style.display = "flex";
 			modalImg.src = this.src;
-			modalImg.style.width = "80vw";
+			modalImg.style.width = "90vw";
 			document.documentElement.classList.add("modal-open");
 			document.body.classList.add("modal-open");
+			// eslint-disable-next-line no-undef
 			lenis.stop();
 		});
 	});
